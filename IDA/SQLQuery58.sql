d08e370ee4ed77ac9bc1e0b4b74942734fe7d1a2
Pugianf <pugianf@gmail.com>
2022-02-28 02:08:57 -0300

    query for responses and dates

---
+++ IDA/SQLQuery58.sql
@@ -0,0 +1,16 @@
+USE DBPRDIDAV2
+GO
+
+SELECT * FROM TBResposta
+INNER JOIN TBAtributoQuestionario
+ON TBAtributoQuestionario.IDAtributoQuestionario = TBResposta.IDAtributoQuestionario
+INNER JOIN TBIndicadorEspecificoQuestionario
+ON TBIndicadorEspecificoQuestionario.IDIndicadorEspecificoQuestionario = TBAtributoQuestionario.IDIndicadorEspecificoQuestionario
+INNER JOIN TBIndicadorGlobalQuestionario
+ON TBIndicadorGlobalQuestionario.IDIndicadorGlobalQuestionario = TBIndicadorEspecificoQuestionario.IDIndicadorGlobalQuestionario
+INNER JOIN TBCategoriaQuestionario
+ON TBCategoriaQuestionario.IDCategoriaQuestionario = TBIndicadorGlobalQuestionario.IDCategoriaQuestionario
+INNER JOIN TBQuestionario
+ON TBQuestionario.IDQuestionario = TBCategoriaQuestionario.IDQuestionario
+INNER JOIN TBAvaliacao
+ON TBAvaliacao.IDAvaliacao = TBQuestionario.IDAvaliacao
