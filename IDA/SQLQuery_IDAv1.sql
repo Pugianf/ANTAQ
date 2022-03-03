09bddfcd6d4d932b184eea1695543c88c8a7a4ee
Pugianf <pugianf@gmail.com>
2022-03-03 13:00:32 -0300

    update

---
+++ IDA/SQLQuery_IDAv1.sql
@@ -0,0 +1,14 @@
+USE DBPRDIDA
+GO
+
+SELECT * FROM TBResposta
+INNER JOIN TBAtributoQuestionario
+ON TBAtributoQuestionario.IDAtributoQuestionario = TBResposta.IDAtributoQuestionario
+INNER JOIN TBIndicadorEspecificoQuestionario
+ON TBIndicadorEspecificoQuestionario.IDIndicadorEspecificoQuestionario = TBAtributoQuestionario.IDIndicadorEspecificoQuestionario
+INNER JOIN TBIndicadorGeralQuestionario
+ON TBIndicadorGeralQuestionario.IDIndicadorGeralQuestionario = TBIndicadorEspecificoQuestionario.IDIndicadorGeralQuestionario
+INNER JOIN TBCategoriaQuestionario
+ON TBCategoriaQuestionario.IDCategoriaQuestionario = TBIndicadorGeralQuestionario.IDCategoriaQuestionario
+INNER JOIN TBQuestionario
+ON TBQuestionario.IDQuestionario = TBCategoriaQuestionario.IDQuestionario
